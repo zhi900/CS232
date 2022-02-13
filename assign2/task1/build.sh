e8c31e01c7a676bc3d8d230cda7441dff703a314
Jonathan wang <65941800+zhi900@users.noreply.github.com>
2022-02-12 20:46:56 -0500

    Add files via upload

---
+++ assign2/task1/build.sh
@@ -0,0 +1,6 @@
+
+cd  assign2/task1
+gcc -std=c11 -Wall intro.c 
+./a.out
+gcc -std=c11 -Wall intro.c -o intro
+./intro < intro.in > intro.out
---
+++ assign2/task1/intro.out
@@ -0,0 +1,2 @@
+Enter radius (in inches):
+Circle's area is 12.57 (sq in).
