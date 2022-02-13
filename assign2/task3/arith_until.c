9db39d286ad58d99ee3c0f502d8662f7d9f83bd3
Jonathan wang <65941800+zhi900@users.noreply.github.com>
2022-02-12 19:33:06 -0500

    Create arith_until.c

---
+++ assign2/task3/arith_until.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+#define PI 3.14159
+
+int main()
+{
+  float r, a, c;
+  while(r!=0){
+  printf("Enter radius (in millimeter):\n");
+  scanf("%f", &r);
+
+  a = PI * (r/25.4) * (r/25.4);
+  c = PI * 2 * r;
+
+  printf("Circle's area is %3.2f (sq in).\n", a);
+  printf("its circumference is %3.2f (in).\n", c/25.4);
+  }
+  printf("Exit");
+}
