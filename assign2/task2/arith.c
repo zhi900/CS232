5b80588a0fa369a1641f6c48ec95273501f7e77c
Jonathan wang <65941800+zhi900@users.noreply.github.com>
2022-02-12 20:47:10 -0500

    Add files via upload

---
+++ assign2/task2/arith.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#define PI 3.14159
+
+int main()
+{
+  float r, a, c;
+
+  printf("Enter radius (in mm):\n");
+  scanf("%f", &r);
+
+  a = PI * (r/25.4) * (r/25.4);
+  c = PI * 2 * r;
+
+  printf("Circle's area is %3.2f (sq in).\n", a);
+  printf("its circumference is %3.2f (in).\n", c/25.4);
+}
