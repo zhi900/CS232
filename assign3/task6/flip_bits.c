c92dfc2ebf92c691aadeb79d93f27701f879f26b
Jonathan wang <65941800+zhi900@users.noreply.github.com>
2022-02-20 21:31:10 -0500

    Create flip_bits.c

---
+++ assign3/task6/flip_bits.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Note, the bits are counted from right to left. 
+// Flip the bits of x within range [start, end], in which both are inclusive.
+// Assume 0 <= start & end <= 31
+void flip_bits(unsigned * x,
+              unsigned start,
+              unsigned end) {
+    for(int i =start;i<=end;++i){
+      (*x) ^= (1 << i);
+    }
+}
