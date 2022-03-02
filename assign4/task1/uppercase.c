68ae873c5522d88fc394959da2faa09067ae8437
Jonathan wang <65941800+zhi900@users.noreply.github.com>
2022-03-02 00:48:10 -0500

    Update uppercase.c

--- assign4/task1/uppercase.c
+++ assign4/task1/uppercase.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+#include <ctype.h>
+ int main(void)
+ {
+   char str[1000];
+   int ch, n = 0;
+  printf("Please, provide input: ");
+   while ((ch = getchar()) != EOF && n < 1000) {
+      str[n] = ch;
+      ++n;
+   }
+   
+   for (int i = 0; i < n; ++i){
+     
+      if(!isdigit(str[i])){
+        str[i] &= ~' ';
+      }
+      putchar(str[i]);
+   }
+      
+   
+    /* trailing '\n' needed in Standard C */
 
+   return 0;
+ }
