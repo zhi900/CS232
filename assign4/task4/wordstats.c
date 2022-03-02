ad12d500f644cee6021e79d2128c101c3dd01267
Jonathan wang <65941800+zhi900@users.noreply.github.com>
2022-03-02 01:16:06 -0500

    Update wordstats.c

--- assign4/task4/wordstats.c
+++ assign4/task4/wordstats.c
@@ -1,1 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+#define MAX_BUF 1024
+
+int main () {
+
+  /* Zero out the array */
+  int letter_frequency[26] = {0};
+  int len, i;
+  char buf[MAX_BUF];
+
+  do {
+     if (scanf("%[^\n]s", buf) == 1){
+       len = strlen(buf);
+     }
+    else{
+      len = 0;
+    }
+     for (int j = 0; j < len; ++j){
+        if (buf[j] >= 'A' && buf[j] <= 'Z'){
+          letter_frequency[buf[j] - 'A']++;
+        }
+       else if(buf[j] >= 'a' && buf[j] <= 'z') {
+         letter_frequency[buf[j] - 'a']++;
+       }
+       }
+     getchar();
+  } while (len > 1);
+
+  printf("Distribution of letters in corpus:\n");
+  for (i = 0; i < 26; i++) {
+    // 'A' is ASCII code 65
+    printf("%c: %d\n", 65+i, letter_frequency[i]);
+  }
+}
 
