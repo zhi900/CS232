c21dc67ab28da6dc1bd44b7aae41f660dee40c95
Jonathan wang <65941800+zhi900@users.noreply.github.com>
2022-03-03 00:45:03 -0500

    Update bubble.c

--- assign4/task4/bubble.c
+++ assign4/task4/bubble.c
@@ -1,52 +1,59 @@
 #include <stdio.h>  
 #include <string.h> 
-
+#include <stdlib.h>
 
 #define NUM 30   /* number of strings */
 #define LEN 1200  /* max length of each string */
-
+void SortNames(char** pStrings, int size)
+{
+  for (int j = 0; j < size -1; j++) {
+    for (int i = 0; i < size -1; i++) {
+      if (0 < strcmp(pStrings[i], pStrings[i +1])) {
+        char* temp = pStrings[i+1];
+        pStrings[i+1] = pStrings[i];
+        pStrings[i] = temp;
+      }
+    }
+  }
+}
 int main()
 {
   char * Strings[NUM];
+  
   int j;
   
   printf("Please enter %d strings, one per line:\n", NUM);
     
   for(int i=0;i<NUM;i++){
+    Strings[i] = malloc(NUM);
+    fgets(Strings[i], LEN-2, stdin);
     
-    fgets(&Strings[i], LEN-2, stdin);
-    if((strcmp(&Strings[i], "\n"))==0){
-      printf("its a blank line");
-      break;
-    }
     j++;
     
+    
+    
   }
+  
+    
    puts("\nHere are the strings in the order you entered:");
 
-    for(int i=0;i<j;i++){
+    for(int i=0;i<NUM;i++){
     
-    printf("%s\n",&Strings[i]);
-    
-  }
-   for(int i =0;i<j;i++){
-     for(int k=0;k<j;k++){
-       if(strcmp(&Strings[k], &Strings[k+1]) >0){
-         void*temp = Strings[k+1];
-         Strings[k+1] = Strings[k];
-         Strings[k] = temp;
-         
-         
-       }
-     }
-   }
-    puts("\nIn alphabetical order, the strings are:");   
-  for(int i=0;i<=j;i++){
-    
-    printf("%s\n",&Strings[i]);
+    printf("%s\n",Strings[i]);
     
   }
+   
+ SortNames(Strings, NUM);
+  puts("\nIn alphabetical order, the strings are:");     
+ 
+  for(int i = 0;i<NUM;i++){
+    
+    printf("%s\n",Strings[i]);
+  }       
+         
+   
 
+ 
   
 
 
